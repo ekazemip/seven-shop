d3f6ab329acf3df4a4a6212ab84fee7d92f6547b
esika <esikataheri9@gmail.com>
2026-09-12 19:58:00 +0330

    postgresql configuration

--- content/urls.py
+++ content/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-
+from .views import HomeView
 app_name = 'home'
 
 urlpatterns = [
-    path("/", HomeView.as_view(), name="home")
+    path("", HomeView.as_view(), name="home")
 ]
--- content/views.py
+++ content/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django import views
 # Create your views here.
 class HomeView(views.View):
-    pass
+    def get(self,request):
+        pass
+    def post(self,request):
+        pass
     
